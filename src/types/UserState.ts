8294064cce9c0edf900107773518083181a58b0c
Joshua Burt <joshburt@shapeandshare.com>
2022-09-17 10:59:53 -0700

    UserFeatureState instead of FeatureDetailsType

--- src/types/UserState.ts
+++ src/types/UserState.ts
@@ -1,15 +1,15 @@
-import { FeatureDetailsType } from './FeatureDetailsType'
 import { StoreType } from './StoreType'
 import { FeatureType } from './FeatureType'
 import { UserStore } from './UserStore'
 import { UserNotification } from './UserNotification'
+import { UserFeatureState } from './UserFeatureState'
 
 export interface UserState {
   active: boolean
   stores: Record<StoreType, UserStore>
   incomes: Record<StoreType, UserStore>
   features: Set<FeatureType>
-  activeFeatureState: FeatureDetailsType
+  activeFeatureState: UserFeatureState
   population: number
   merchants: Set<StoreType>
   notifications: UserNotification[]
